5458f514418fb160354ff9299c184714c64785c6
Bartosz <bartosz.slowik@student.pk.edu.pl>
2022-11-12 03:57:54 +0100

    adding forgotten summary function  to a task from 26.10 COSMO meeting

--- exercises/intermediate/ex2/src/main.rs
+++ exercises/intermediate/ex2/src/main.rs
@@ -1,52 +1,36 @@
-use std::path::PathBuf;
 use std::env;
 use std::fs;
 use std::io;
+use std::path::PathBuf;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let mut oceny =  Vec::new();
-
-    dbg!(&args);
-
+    //dbg!(&args);
     let entries = fs::read_dir(args[1].to_string()).unwrap();
+    let mut ratings: Vec<(PathBuf, u32)> = Vec::new();
 
     for entry in entries {
         let path = entry.expect("").path();
         println!("Name: {}", path.display());
-        
-        let contents = fs::read_to_string(path)
-            .expect("ERROR PLIKU");
+        let contents = fs::read_to_string(&path).expect("ERROR FILE");
+        clear_terminal();
         println!("{contents}");
-        println!("Oceń plik.");
+        println!("Rate file.");
         let mut guess = String::new();
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
-            oceny.push(guess.trim().parse::<u32>().unwrap());
-            
+        ratings.push((path, guess.trim().parse::<u32>().unwrap()));
     }
-    dbg!(oceny);
-
-    
-    
-
-
-    //let dir_content = todo!("get contents of the directory");
-
-    // `Vec<(PathBuf, u32)>` is a dynamically sized array
-    // of tuples containing `PathBuf` and `u32`
-   /* let ratings: Vec<(PathBuf, u32)> = todo!("
-        for entry in dir_content
-            clear the screen
-            present contents of a file to the user
-            ask user to input his rating from a 1-5 scale and store it in `ratings`,
-    ");
-    */
-    
-    //todo!("Present the ratings summary to the user");
+    show_summary(ratings)
 }
-
+fn show_summary(ratings: Vec<(PathBuf, u32)>) {
+    clear_terminal();
+    println!("Summary: ");
+    for x in ratings {
+        println!("{:20} -> {:2} ", x.0.display(), x.1);
+    }
+}
 fn clear_terminal() {
     print!("\x1B[2J\x1B[1;1H");
 }
