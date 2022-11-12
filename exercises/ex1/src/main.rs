d1a442ef6f75c5121aac0379687110adf02ab26c
Bartosz <bartosz.slowik@student.pk.edu.pl>
2022-11-12 02:39:47 +0100

    Exercise 1 done on cosmo meeting.

--- exercises/ex1/src/main.rs
+++ exercises/ex1/src/main.rs
@@ -1,20 +1,50 @@
 use std::path::PathBuf;
+use std::env;
+use std::fs;
+use std::io;
 
 fn main() {
-    let dir_path = todo!("read directory path from command line arguments");
+    let args: Vec<String> = env::args().collect();
+    let mut oceny =  Vec::new();
 
-    let dir_content = todo!("get contents of the directory");
+    dbg!(&args);
+
+    let entries = fs::read_dir(args[1].to_string()).unwrap();
+
+    for entry in entries {
+        let path = entry.expect("").path();
+        println!("Name: {}", path.display());
+        
+        let contents = fs::read_to_string(path)
+            .expect("ERROR PLIKU");
+        println!("{contents}");
+        println!("Oceń plik.");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+            oceny.push(guess.trim().parse::<u32>().unwrap());
+            
+    }
+    dbg!(oceny);
+
+    
+    
+
+
+    //let dir_content = todo!("get contents of the directory");
 
     // `Vec<(PathBuf, u32)>` is a dynamically sized array
     // of tuples containing `PathBuf` and `u32`
-    let ratings: Vec<(PathBuf, u32)> = todo!("
+   /* let ratings: Vec<(PathBuf, u32)> = todo!("
         for entry in dir_content
             clear the screen
             present contents of a file to the user
             ask user to input his rating from a 1-5 scale and store it in `ratings`,
     ");
+    */
     
-    todo!("Present the ratings summary to the user");
+    //todo!("Present the ratings summary to the user");
 }
 
 fn clear_terminal() {
